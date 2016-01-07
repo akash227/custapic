32a464057b7339e754fbcc810ac5b5aeb24e1dbd
akash227 <akash227@yahoo.com>
2016-01-07 23:28:58 +0530

    Objectifing Photos in tumblr.js

--- api/tumblr.js
+++ api/tumblr.js
@@ -7,12 +7,17 @@ var tumblr = new Tumblr.Tagged(oauth);
 //search
 
 tumblr.search('Choco',{limit:10},function(error,results){
-	
+	var photosObject = {photosArray: []};
 	for(index in results){
 		var photos=results[index].photos;
+		var title = results[index].blog_name;
+		var source = "tumblr";
 		for(photo in photos){
 			//gives the url of photo
-			console.log(photos[photo].original_size.url);
+			var url = photos[photo].original_size.url;
+			var pht = {title: title,source: source,url: url};
+			photosObject.photosArray.push(photo);
 		}
 	}
+	console.log(photosObject);
 });
