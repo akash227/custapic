04fb941bb87dfa2f44a57018b9a1e6009e02a62b
akash227 <akash227@yahoo.com>
2016-01-07 23:10:38 +0530

    Adding search functionality to tubmlr.js

--- api/tumblr.js
+++ api/tumblr.js
@@ -2,4 +2,10 @@ var Tumblr = require('tumblr');
 var keys = require('./keys.js')
 var oauth = keys.tumblrOauth() ;
 
-var tumble = new Tumblr.Tagged(oauth);
+var tumblr = new Tumblr.Tagged(oauth);
+
+//search
+
+tumblr.search('Choco',{limit:10},function(error,results){
+	console.log(results);
+});
