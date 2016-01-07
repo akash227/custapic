489c31404f0f2f00e9a70b4d9f8cfa2252ca95a0
akash227 <akash227@yahoo.com>
2016-01-07 23:35:30 +0530

    Modularising tumblr.js

--- api/tumblr.js
+++ api/tumblr.js
@@ -6,7 +6,10 @@ var tumblr = new Tumblr.Tagged(oauth);
 
 //search
 
-tumblr.search('Choco',{limit:10},function(error,results){
+function search ( query, next){
+	
+	tumblr.search(query,{limit:10},function(error,results){
+		if(error) next(error,null);
 	var photosObject = {photosArray: []};
 	for(index in results){
 		var photos=results[index].photos;
@@ -16,8 +19,17 @@ tumblr.search('Choco',{limit:10},function(error,results){
 			//gives the url of photo
 			var url = photos[photo].original_size.url;
 			var pht = {title: title,source: source,url: url};
-			photosObject.photosArray.push(photo);
+			photosObject.photosArray.push(pht);
 		}
 	}
-	console.log(photosObject);
+	next(null, photosObject);
 });
+
+
+}
+
+// search('McDonald',function(err,res){
+// 	console.log(res);
+// });
+
+exports.search = search;
