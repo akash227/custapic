c29f369c6ba41ab22ad7147e76d71b65c17f547b
akash227 <akash227@yahoo.com>
2016-01-12 17:44:57 +0530

    Updating index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var Hapi = require('hapi');
+var Good = require('Good')
 var api = require('./api/search.js');
 var server = new Hapi.Server();
 
@@ -33,8 +34,27 @@ server.route({
         });
     }
 });
+
 server.method({name: 'search',method: search});
-//starting the server.
-server.start(() => {
-    console.log('Server running at:', server.info.uri);
+
+server.register({
+    register: Good,
+    options: {
+        reporters: [{
+            reporter: require('good-console'),
+            events: {
+                response: '*',
+                log: '*'
+            }
+        }]
+    }
+}, function (err) {
+    if (err) {
+        throw err; // something bad happened loading the plugin
+    }
+
+    server.start(function () {
+        server.log('info', 'Server running at: ' + server.info.uri);
+    });
 });
+
